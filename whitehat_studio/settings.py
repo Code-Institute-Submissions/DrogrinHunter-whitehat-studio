bfde4b47e41b3a37b9f8bbd7306b98ee459212d0
DrogrinHunter <lukewillmore67@gmail.com>
2021-12-22 22:38:45 +0000

    added checkout.html code, installed crispy forms, resolved issue in urls.py where checkout had an extra s

--- whitehat_studio/settings.py
+++ whitehat_studio/settings.py
@@ -52,6 +52,10 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
+    
 ]
 
 MIDDLEWARE = [
@@ -65,6 +69,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'whitehat_studio.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,8 +86,14 @@ TEMPLATES = [
                 'django.template.context_processors.request', # This is required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
