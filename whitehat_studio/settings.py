975312593339828b4ab9a30cdf0c9fe92e16d65e
DrogrinHunter <lukewillmore67@gmail.com>
2021-12-31 00:21:26 +0000

    turned debug to true as I am getting server 500 on the bag and profile

--- whitehat_studio/settings.py
+++ whitehat_studio/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEVELOPMENT" in os.environ
+DEBUG = True
+# "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["whitehat-studio.herokuapp.com", "localhost"]
 
@@ -122,7 +123,7 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
